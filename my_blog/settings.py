444718ae3e641bc4ad91951ee4d343ac175f0fb3
Aniche Enare <henryenare001@gmail.com>
2020-03-10 12:40:52 +0100

    updated

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'e55b9&7=r!9!juy35#%#z5*+vhqt)92j0($=+5&o&dzppksc_w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
