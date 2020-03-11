fdc8018e907d03d00eb60abc2bc97d2df4b8446b
Aniche Enare <henryenare001@gmail.com>
2020-03-11 14:29:40 +0100

    update

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -6,6 +7,22 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
     body = models.TextField()
 
+    def get_absolute_url(self):
+        return reverse('detail', args = [str(self.id)])
+
+
     def __str__(self):
         return self.title
+
+class Comments(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('detail', args = [str(self.id)])
+ 
+    
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
+from .models import Post, Comments
 
 # Create your views here.
 class PostListView(ListView):
@@ -14,4 +16,18 @@ class PostDetailView(DetailView):
     model = Post
     context_object_name = 'details'
 
-     
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('home')
